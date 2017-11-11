df4ad10c714f9ffc658db8338eb8d5f32416515b
Wikotr <33160336+wikotr@users.noreply.github.com>
2017-11-11 13:30:57 +0100

    Engine Class: basic fields and converter from HP to kW

--- CarAssistant/Classes/Car/Engine.cs
+++ CarAssistant/Classes/Car/Engine.cs
@@ -8,10 +8,27 @@ namespace CarAssistant
 {
 	public class Engine
 	{
+        //Fields
+        int Capacity;
+        int Horsepower;
+        int PowerInKW;
+        
+        // Constructor
+        public Engine()
+        {
 
-		public Engine()
-		{
+        }
 
-		}
+        // Methods
+
+        // HorsepowerTokW - Converts Power of the engine from Horsepower to kiloWatts
+        private double HorsepowerTokW (int Horsepower) 
+        {
+            double Converter = 0.745699872;
+            double kW = (Horsepower * Converter);
+            return kW;
+        }
+
+
 	}
 }
