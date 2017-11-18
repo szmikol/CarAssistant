7d7246e761e4fee5c484feed1581df719bfe1773
Kalik93 <kacper.kalinski93@gmail.com>
2017-11-18 14:31:26 +0100

    Started work on Driver Panel.

--- CarAssistant/Form1.Designer.cs
+++ CarAssistant/Form1.Designer.cs
@@ -60,13 +60,28 @@
             this.panelSettings = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
             this.panelSaveLoad = new System.Windows.Forms.Panel();
+            this.bLoadFile = new System.Windows.Forms.Button();
+            this.bSaveFile = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.bSaveFile = new System.Windows.Forms.Button();
-            this.bLoadFile = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panelCentral = new System.Windows.Forms.Panel();
+            this.picUserPhoto = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.tbDrPName = new System.Windows.Forms.TextBox();
+            this.tbDrPAge = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panelMenu.SuspendLayout();
             this.panelButtons.SuspendLayout();
+            this.panelDriverInfo.SuspendLayout();
             this.panelCars.SuspendLayout();
             this.panelManage.SuspendLayout();
             this.panelService.SuspendLayout();
@@ -76,6 +91,8 @@
             this.panelReminders.SuspendLayout();
             this.panelSettings.SuspendLayout();
             this.panelSaveLoad.SuspendLayout();
+            this.panelCentral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -337,10 +354,23 @@
             // panelDriverInfo
             // 
             this.panelDriverInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.panelDriverInfo.Controls.Add(this.textBox1);
+            this.panelDriverInfo.Controls.Add(this.tbDrPAge);
+            this.panelDriverInfo.Controls.Add(this.tbDrPName);
+            this.panelDriverInfo.Controls.Add(this.label16);
+            this.panelDriverInfo.Controls.Add(this.label15);
+            this.panelDriverInfo.Controls.Add(this.label14);
+            this.panelDriverInfo.Controls.Add(this.label13);
+            this.panelDriverInfo.Controls.Add(this.label12);
+            this.panelDriverInfo.Controls.Add(this.label11);
+            this.panelDriverInfo.Controls.Add(this.label10);
+            this.panelDriverInfo.Controls.Add(this.label9);
+            this.panelDriverInfo.Controls.Add(this.label8);
+            this.panelDriverInfo.Controls.Add(this.picUserPhoto);
             this.panelDriverInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelDriverInfo.Location = new System.Drawing.Point(200, 0);
             this.panelDriverInfo.Name = "panelDriverInfo";
-            this.panelDriverInfo.Size = new System.Drawing.Size(900, 100);
+            this.panelDriverInfo.Size = new System.Drawing.Size(900, 150);
             this.panelDriverInfo.TabIndex = 1;
             // 
             // panelCars
@@ -502,10 +532,32 @@
             this.panelSaveLoad.Controls.Add(this.bSaveFile);
             this.panelSaveLoad.Controls.Add(this.label7);
             this.panelSaveLoad.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelSaveLoad.Location = new System.Drawing.Point(0, 0);
+            this.panelSaveLoad.Location = new System.Drawing.Point(200, 150);
             this.panelSaveLoad.Name = "panelSaveLoad";
-            this.panelSaveLoad.Size = new System.Drawing.Size(1100, 600);
+            this.panelSaveLoad.Size = new System.Drawing.Size(900, 450);
             this.panelSaveLoad.TabIndex = 8;
+            // 
+            // bLoadFile
+            // 
+            this.bLoadFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bLoadFile.Location = new System.Drawing.Point(490, 200);
+            this.bLoadFile.Name = "bLoadFile";
+            this.bLoadFile.Size = new System.Drawing.Size(200, 80);
+            this.bLoadFile.TabIndex = 2;
+            this.bLoadFile.Text = "Load File";
+            this.bLoadFile.UseVisualStyleBackColor = true;
+            this.bLoadFile.Click += new System.EventHandler(this.bLoadFile_Click);
+            // 
+            // bSaveFile
+            // 
+            this.bSaveFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bSaveFile.Location = new System.Drawing.Point(252, 200);
+            this.bSaveFile.Name = "bSaveFile";
+            this.bSaveFile.Size = new System.Drawing.Size(200, 80);
+            this.bSaveFile.TabIndex = 1;
+            this.bSaveFile.Text = "Save File";
+            this.bSaveFile.UseVisualStyleBackColor = true;
+            this.bSaveFile.Click += new System.EventHandler(this.bSaveFile_Click);
             // 
             // label7
             // 
@@ -521,27 +573,163 @@
             this.openFileDialog1.DefaultExt = "xml";
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // bSaveFile
+            // panelCentral
             // 
-            this.bSaveFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bSaveFile.Location = new System.Drawing.Point(450, 300);
-            this.bSaveFile.Name = "bSaveFile";
-            this.bSaveFile.Size = new System.Drawing.Size(200, 80);
-            this.bSaveFile.TabIndex = 1;
-            this.bSaveFile.Text = "Save File";
-            this.bSaveFile.UseVisualStyleBackColor = true;
-            this.bSaveFile.Click += new System.EventHandler(this.bSaveFile_Click);
+            this.panelCentral.Controls.Add(this.panelSaveLoad);
+            this.panelCentral.Controls.Add(this.panelDriverInfo);
+            this.panelCentral.Controls.Add(this.panelMenu);
+            this.panelCentral.Controls.Add(this.panelCars);
+            this.panelCentral.Controls.Add(this.panelService);
+            this.panelCentral.Controls.Add(this.panelManage);
+            this.panelCentral.Controls.Add(this.panelInsurance);
+            this.panelCentral.Controls.Add(this.panelRefuel);
+            this.panelCentral.Controls.Add(this.panelExpenses);
+            this.panelCentral.Controls.Add(this.panelReminders);
+            this.panelCentral.Controls.Add(this.panelSettings);
+            this.panelCentral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCentral.Location = new System.Drawing.Point(0, 0);
+            this.panelCentral.Name = "panelCentral";
+            this.panelCentral.Size = new System.Drawing.Size(1100, 600);
+            this.panelCentral.TabIndex = 3;
             // 
-            // bLoadFile
+            // picUserPhoto
             // 
-            this.bLoadFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bLoadFile.Location = new System.Drawing.Point(688, 300);
-            this.bLoadFile.Name = "bLoadFile";
-            this.bLoadFile.Size = new System.Drawing.Size(200, 80);
-            this.bLoadFile.TabIndex = 2;
-            this.bLoadFile.Text = "Load File";
-            this.bLoadFile.UseVisualStyleBackColor = true;
-            this.bLoadFile.Click += new System.EventHandler(this.bLoadFile_Click);
+            this.picUserPhoto.Dock = System.Windows.Forms.DockStyle.Right;
+            this.picUserPhoto.Location = new System.Drawing.Point(750, 0);
+            this.picUserPhoto.Name = "picUserPhoto";
+            this.picUserPhoto.Size = new System.Drawing.Size(150, 150);
+            this.picUserPhoto.TabIndex = 0;
+            this.picUserPhoto.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(349, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Driver";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(27, 36);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 17);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Name:";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(43, 75);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 17);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Age";
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(6, 113);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(73, 17);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Birth date:";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(255, 33);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(64, 17);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Address:";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(395, 33);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(33, 17);
+            this.label13.TabIndex = 6;
+            this.label13.Text = "Age";
+            // 
+            // label14
+            // 
+            this.label14.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label14.AutoSize = true;
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(524, 33);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(79, 17);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "ID Number:";
+            // 
+            // label15
+            // 
+            this.label15.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label15.AutoSize = true;
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(524, 100);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(87, 17);
+            this.label15.TabIndex = 8;
+            this.label15.Text = "Owned cars:";
+            // 
+            // label16
+            // 
+            this.label16.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label16.AutoSize = true;
+            this.label16.ForeColor = System.Drawing.Color.White;
+            this.label16.Location = new System.Drawing.Point(524, 72);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(113, 17);
+            this.label16.TabIndex = 9;
+            this.label16.Text = "Licence number:";
+            // 
+            // tbDrPName
+            // 
+            this.tbDrPName.Location = new System.Drawing.Point(82, 33);
+            this.tbDrPName.Name = "tbDrPName";
+            this.tbDrPName.Size = new System.Drawing.Size(167, 22);
+            this.tbDrPName.TabIndex = 10;
+            // 
+            // tbDrPAge
+            // 
+            this.tbDrPAge.Location = new System.Drawing.Point(82, 72);
+            this.tbDrPAge.Name = "tbDrPAge";
+            this.tbDrPAge.Size = new System.Drawing.Size(167, 22);
+            this.tbDrPAge.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
+            this.textBox1.TabIndex = 12;
             // 
             // Form1
             // 
@@ -549,17 +737,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1100, 600);
-            this.Controls.Add(this.panelDriverInfo);
-            this.Controls.Add(this.panelMenu);
-            this.Controls.Add(this.panelSaveLoad);
-            this.Controls.Add(this.panelSettings);
-            this.Controls.Add(this.panelReminders);
-            this.Controls.Add(this.panelExpenses);
-            this.Controls.Add(this.panelRefuel);
-            this.Controls.Add(this.panelInsurance);
-            this.Controls.Add(this.panelCars);
-            this.Controls.Add(this.panelService);
-            this.Controls.Add(this.panelManage);
+            this.Controls.Add(this.panelCentral);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
@@ -568,6 +746,8 @@
             this.Text = "Car Assistant v0.1";
             this.panelMenu.ResumeLayout(false);
             this.panelButtons.ResumeLayout(false);
+            this.panelDriverInfo.ResumeLayout(false);
+            this.panelDriverInfo.PerformLayout();
             this.panelCars.ResumeLayout(false);
             this.panelCars.PerformLayout();
             this.panelManage.ResumeLayout(false);
@@ -586,6 +766,8 @@
             this.panelSettings.PerformLayout();
             this.panelSaveLoad.ResumeLayout(false);
             this.panelSaveLoad.PerformLayout();
+            this.panelCentral.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto)).EndInit();
             this.ResumeLayout(false);
 
 		}
@@ -629,6 +811,20 @@
         private System.Windows.Forms.Button bLoadFile;
         private System.Windows.Forms.Button bSaveFile;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panelCentral;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox picUserPhoto;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox tbDrPName;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbDrPAge;
     }
 }
 
