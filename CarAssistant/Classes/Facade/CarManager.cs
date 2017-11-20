56bab62a51a3fcba21b3bec7ea8373ee6ddc6e3b
Wikotr <33160336+wikotr@users.noreply.github.com>
2017-11-20 21:25:59 +0100

    same in CarManager

--- CarAssistant/Classes/Facade/CarManager.cs
+++ CarAssistant/Classes/Facade/CarManager.cs
@@ -10,47 +10,47 @@ namespace CarAssistant
 {
 	public class CarManager : ICarManager
 	{
-		public void CreateNewCar(Car NewCar)
+		public void CreateNewCar(User WhichUser, Car NewCar)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void DeleteCar(Car CarToDelete)
+		public void DeleteCar(User WhichUser, Car CarToDelete)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void DeleteCarById(int Id)
+		public void DeleteCarById(User WhichUser, int Id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void DeleteCarByModel(Brand Brand, Model CarModel)
+		public void DeleteCarByModel(User WhichUser, Brand Brand, Model CarModel)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Car FindCarById(int Id)
+		public Car FindCarById(User WhichUser, int Id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public List<Car> FindCardByModel(Model CarModel)
+		public List<Car> FindCardByModel(User WhichUser, Model CarModel)
 		{
 			throw new NotImplementedException();
 		}
 
-		public List<Car> FindCarsByBrand(Brand CarBrand)
+		public List<Car> FindCarsByBrand(User WhichUser, Brand CarBrand)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Dictionary<Brand, Model> RetrieveAllCarsOfSpecificBrand(Brand CarBrand)
+		public Dictionary<Brand, Model> RetrieveAllCarsOfSpecificBrand(User WhichUser, Brand CarBrand)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Car UpdateCar(Car UpdatedCar)
+		public Car UpdateCar(User WhichUser, Car UpdatedCar)
 		{
 			throw new NotImplementedException();
 		}
