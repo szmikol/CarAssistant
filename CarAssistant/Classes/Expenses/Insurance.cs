7f782c1b5cd71cd936d65122cc36f29704f904cf
Kalik93 <kacper.kalinski93@gmail.com>
2017-11-29 20:00:08 +0100

    Added Expenses structure

---
+++ CarAssistant/Classes/Expenses/Insurance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAssistant.Classes.Expenses
+{
+    public class Insurance : Expense
+    {
+        private bool HirePurchase;
+        public DateTime ExpirationDate { get; set; }
+        public string WhichInsurer { get; set; }
+        public Insurance(decimal cost, string description, DateTime when, bool hirePurchase, DateTime expirationDate, string whichInsurer)
+        {
+            Cost = cost;
+            Description = description;
+            When = when;
+            HirePurchase = hirePurchase;
+            ExpirationDate = expirationDate;
+            WhichInsurer = whichInsurer;
+        }
+    }
+}
